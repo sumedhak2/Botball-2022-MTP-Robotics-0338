c9ac7aa3db956b201afa513a4424ce75e323f5c1
sumedhak2 <87838878+sumedhak2@users.noreply.github.com>
2022-03-27 19:40:44 -0700

    Changed code for Botguy to go to transporter
    Adjusted the destination after grabbing botguy. Made it drop Botguy to the transporter instead of airlock.

--- main.c
+++ main.c
@@ -1,69 +1,51 @@
 
 #include <kipr/wombat.h>
-// void enable_servo(0);
-// void enable_servo(3);
+
 int main()
 {
-   
-	//initial setup
     create_connect();
-    set_servo_position(3, 1300);
-    
-    
-    create_drive_direct(200,200);
-    msleep(4500);
-    set_create_total_angle(0);
-    create_spin_CCW(90);
-    msleep(2100);
     enable_servo(0);
     enable_servo(3);
-    int i;
     
-    for(i = 1000; i > 75; i -= 9) {
+    //Going in line with Botguy
+    set_servo_position(3, 2047);
+    msleep(1100);
+    create_drive_direct(200,200);
+    msleep(4600);
+    set_create_total_angle(0);
+    create_spin_CCW(80);
+    msleep(2050);
+    
+    //Lifting Arm
+    int i;
+    for(i = 2047; i > 1000; i -= 9) {
         set_servo_position(0, i);
     }
     
+    //Grabing BOTBALL and going bcack
     create_drive_direct(200,200);
-    msleep(3480);
+    msleep(4400);
+    set_servo_position(3, 450);
     msleep(500);
-    set_servo_position(0, 100);
-    msleep(500);
-    set_servo_position(3, 200);    
-    msleep(1000);
     create_drive_direct(-200,-200);
-    msleep(4020);
+    msleep(2500);
     
+    //:)
+
+    //Going to Transporter and dropping it
     set_create_total_angle(0);
     create_spin_CW(170);
+    msleep(1050);
+    create_drive_direct(200, 200);
+    msleep(1800);
+    set_servo_position(0, 1700);
     msleep(1000);
-    create_drive_direct(200, 200);
-    msleep(3150);
-    create_spin_CW(50);
-    msleep(500);
-    create_drive_direct(200, 200);
-    msleep(600);
-    set_servo_position(0, 220);
-    msleep(550);
-    
-    set_servo_position(3, 1300);
-    
+    create_drive_direct(100,100);
+    msleep(300);
+    set_servo_position(3, 2047);
+    msleep(200);
+    set_servo_position(0, 1100);
+    msleep(200);
     create_disconnect();
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
