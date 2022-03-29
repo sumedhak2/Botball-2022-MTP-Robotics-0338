33d21d2fcfad9e540a1eff1f9cc2a5dd19c1f35f
PeaceMushyYT <86090471+PeaceMushyYT@users.noreply.github.com>
2022-03-28 19:04:52 -0700

    Added Line Follower
    Added line follower to go back to robot staging area to score points with the rack-a-stack.

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    create_connect();
+   create_connect();
     enable_servo(0);
     enable_servo(3);
     
@@ -27,22 +27,49 @@ int main()
     set_servo_position(3, 450);
     msleep(500);
     create_drive_direct(-200,-200);
-    msleep(2700);
+    msleep(2800);
     
     //:)
    
     //Going to Transporter and dropping it
     set_create_total_angle(0);
     create_spin_CW(170);
-    msleep(950);
+    msleep(1000);
     create_drive_direct(200, 200);
     msleep(2600);
     create_drive_direct(100,100);
-    msleep(1000);
+    msleep(1400);
+    set_servo_position(3, 2047);
     set_servo_position(0, 1950);
     msleep(200);
-    set_servo_position(3, 2047);
+    set_servo_position(0, 1950);
     msleep(200);
+    
+    
+    // go back to spawn area
+    create_drive_direct(-200, -200);
+    msleep(800);
+    
+    
+    create_spin_CCW(180);
+    msleep(2000); 
+   
+    while (1){
+        
+        if(get_create_lfcliff_amt() > 1200) {
+            create_drive_direct(100,25); 
+        }
+        else if(get_create_rfcliff_amt() > 1200) {
+            create_drive_direct(25,100);
+        }
+        else {
+            break;
+        }
+            
+            
+           
+    }
+    
     create_disconnect();
     return 0;
 }
